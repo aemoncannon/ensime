03abd02ac1c63413cafc607f54c332b94333d4fa
Jan Christopher Vogt <oss.nsp@cvogt.org>
2015-10-24 18:13:44 -0400

    turn knownFiles and removeFiles async

--- core/src/main/scala/org/ensime/indexer/DatabaseService.scala
+++ core/src/main/scala/org/ensime/indexer/DatabaseService.scala
@@ -57,9 +57,9 @@ class DatabaseService(dir: File) extends SLF4JLogging {
   // TODO reverse lookup table
 
   // file with last modified time
-  def knownFiles(): List[FileCheck] = Await.result(db.run(fileChecks.to[List].result), Duration.Inf)
+  def knownFiles(): Future[List[FileCheck]] = db.run(fileChecks.to[List].result)
 
-  def removeFiles(files: List[FileObject])(implicit ec: ExecutionContext): Int = await(
+  def removeFiles(files: List[FileObject])(implicit ec: ExecutionContext): Future[Int] =
     db.run {
       val restrict = files.map(_.getName.getURI)
 
@@ -69,7 +69,6 @@ class DatabaseService(dir: File) extends SLF4JLogging {
         fileChecks.filter(_.filename inSet restrict).delete
       )
     }
-  )
 
   private val timestampsQuery = Compiled {
     filename: Rep[String] => fileChecks.filter(_.filename === filename).map(_.timestamp).take(1)
