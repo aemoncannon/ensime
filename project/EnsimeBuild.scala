9348d402d16a01e6622555c1d66ba2a09f319769
Sam Halliday <sam.halliday@gmail.com>
2016-12-19 21:34:52 +0000

    bump middleware and silence akka

--- project/EnsimeBuild.scala
+++ project/EnsimeBuild.scala
@@ -165,8 +165,8 @@ object EnsimeBuild {
         "com.typesafe.akka" %% "akka-slf4j" % akkaVersion.value,
         scalaBinaryVersion.value match {
           // see notes in https://github.com/ensime/ensime-server/pull/1446
-          case "2.10" => "org.scala-refactoring" % "org.scala-refactoring.library_2.10.6" % "0.11.0-rc2"
-          case "2.11" => "org.scala-refactoring" % "org.scala-refactoring.library_2.11.8" % "0.11.0-rc2"
+          case "2.10" => "org.scala-refactoring" % "org.scala-refactoring.library_2.10.6" % "0.11.0"
+          case "2.11" => "org.scala-refactoring" % "org.scala-refactoring.library_2.11.8" % "0.11.0"
         },
         "commons-lang" % "commons-lang" % "2.6",
         "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0",
@@ -234,7 +234,7 @@ object EnsimeBuild {
 
   private def akkaVersion: Def.Initialize[String] = Def.setting {
     CrossVersion.partialVersion(scalaVersion.value) match {
-      case Some((2, minor)) if minor >= 11 => "2.4.14"
+      case Some((2, minor)) if minor >= 11 => "2.4.16"
       case _ => "2.3.16"
     }
   }
