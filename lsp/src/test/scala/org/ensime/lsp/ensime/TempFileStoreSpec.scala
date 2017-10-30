78e40b3b8831b27e70f935a56d8fd799a7b2e625
vovapolu <vovapolu@gmail.com>
2017-10-30 23:35:53 +0300

    More generic code for tmp storage

--- lsp/src/test/scala/org/ensime/lsp/ensime/TempFileStoreSpec.scala
+++ lsp/src/test/scala/org/ensime/lsp/ensime/TempFileStoreSpec.scala
@@ -2,23 +2,21 @@
 // License: http://www.gnu.org/licenses/gpl-3.0.en.html
 package org.ensime.lsp.ensime
 
+import java.nio.file.{ Files, Paths }
+
 import org.ensime.api.ArchiveFile
 import org.scalatest._
 import org.scalatest.Matchers._
 
-import scala.reflect.io.Path
-
 class TempFileStoreSpec extends FreeSpec {
   "Extraction in TempFileStore" - {
     "should work correctly" in {
-      val tempStore = new TempFileStore("/tmp/testStore")
-
-      // we don't know if we are run inside the root project, or the sub-project
-      val jar  = Path("src") / "test" / "resources" / "test.jar"
-      val jar1 = if (jar.exists) jar else Path("lsp") / jar
-
+      val tempStore = new TempFileStore(
+        Files.createTempDirectory("testStore").toAbsolutePath.toString
+      )
+      val jar = Paths.get(getClass.getResource("/test.jar").toURI)
       val extracted = tempStore.getFile(
-        ArchiveFile(jar1.toAbsolute.jfile.toPath, "/scala/Predef.scala")
+        ArchiveFile(jar, "/scala/Predef.scala")
       )
 
       extracted.isSuccess shouldBe true
