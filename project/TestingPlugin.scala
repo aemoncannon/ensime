9f90619be191cd42853c2fc51843666ac00984e0
Sam Halliday <sam.halliday@gmail.com>
2017-12-06 10:52:27 +0000

    kill. me. now.

--- project/TestingPlugin.scala
+++ project/TestingPlugin.scala
@@ -25,7 +25,7 @@ object TestingPlugin extends AutoPlugin {
 
   override def buildSettings = Seq()
   override def projectSettings = Seq(
-    testFrameworks := Nil,
+    testFrameworks in Test := Nil,
     publishArtifact in (Compile, packageDoc) := false,
     publishArtifact in packageDoc := false,
     sources in (Compile, doc) := Nil,
