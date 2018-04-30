34c186040667b91a1842f4f9250bae65fa76ff74
dickmao <rchiang@cs.stonybrook.edu>
2018-04-29 23:18:01 -0400

    scalafmtcheck fixes

--- json/src/main/scala/spray/json/JsReader.scala
+++ json/src/main/scala/spray/json/JsReader.scala
@@ -60,8 +60,8 @@ object JsReader {
 
   implicit def option[A: JsReader]: JsReader[Option[A]] = {
     case JsAbsent => None
-    case JsNull => None
-    case a      => Some(JsReader[A].read(a))
+    case JsNull   => None
+    case a        => Some(JsReader[A].read(a))
   }
   implicit def either[A: JsReader, B: JsReader]: JsReader[Either[A, B]] = { v =>
     (Try(JsReader[A].read(v)), Try(JsReader[B].read(v))) match {
